404c4f99df6628fa788500dbe9ca6ece7bf1a22b
JiehoonPark <96074027+JiehoonPark@users.noreply.github.com>
2022-08-11 17:58:08 +0900

    deploy on heroku

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./data/db.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 4000;
 
 server.use(middlewares);
 
 server.use(jsonServer.bodyParser);
-
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  next();
+});
 server.use(router);
 server.listen(port, () => {
   console.log("JSON Server is running");
